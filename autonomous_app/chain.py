87341fedfe4269b6624e96c5a27ade7739cd021f
Sanchay Thalnerkar <thalnerkarsanchay17@gmail.com>
2023-08-21 13:08:55 +0530

    embred

--- autonomous_app/chain.py
+++ autonomous_app/chain.py
@@ -6,7 +6,7 @@ import wandb
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from .prompts import load_chat_prompt
 import os 
 from dotenv import load_dotenv
